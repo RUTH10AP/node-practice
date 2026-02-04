caa6a15ee735993917e514c4e46a34f21e68b363
Ruth <wonderuth1gmail.com>
2026-02-03 22:32:55 -0800

    node file

---
+++ nodejs  practice/commonjs/index.js
@@ -0,0 +1,10 @@
+const maths = require("./maths");
+
+console.log(maths.add(3, 5));
+
+// const maths = requir("./maths");
+// console.log(maths.ruth(10, 30));
+
+const maths = require("./maths");
+
+console.log(maths.ruth(10, 30));
---
+++ nodejs  practice/commonjs/maths.js
@@ -0,0 +1,7 @@
+let add = (x, y) => x + y;
+
+module.exports = { add };
+
+let ruth = (x, y) => x + y;
+
+module.exports = { ruth };
